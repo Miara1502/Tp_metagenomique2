26a86acaf3b5ff01278d13a9fb5eeede26b23b3d
miara1502 <miara.rakotomavo@gmail.com>
2019-10-24 19:47:49 +0200

    Assemblage

--- wgs.sh
+++ wgs.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-gunzip EchD_R1.fastq.gz
-gunzip EchD_R2.fastq.gz
+gunzip EchG_R1.fastq.gz
+gunzip EchG_R2.fastq.gz
 mkdir resultats
 
 # 1° 2° Identification et quantification des bactéries
 
 #	# Alignement des reads avec all.genome.fasta
-./soft/bowtie2 --end-to-end --fast -x ./databases/all_genome.fasta -1 EchD_R1.fastq -2 EchD_R2.fastq -S ./resultats/EchD.sam
+./soft/bowtie2 --end-to-end --fast -x ./databases/all_genome.fasta -1 EchG_R1.fastq -2 EchG_R2.fastq -S ./resultats/EchG.sam
 
 #	#Quantifier l'abondance de chaque bactérie
 
-samtools view -b ./resultats/EchD.sam -1 -o ./resultats/EchD.bam
-samtools sort ./resultats/EchD.bam -o ./resultats/EchD_sorted.bam
-samtools index ./resultats/EchD_sorted.bam
-samtools idxstats ./resultats/EchD_sorted.bam
-samtools idxstats ./resultats/EchD_sorted.bam > ./resultats/comptage.txt
+samtools view -b ./resultats/EchG.sam -1 -o ./resultats/EchG.bam
+samtools sort ./resultats/EchG.bam -o ./resultats/EchG_sorted.bam
+samtools index ./resultats/EchG_sorted.bam
+samtools idxstats ./resultats/EchG_sorted.bam
+samtools idxstats ./resultats/EchG_sorted.bam > ./resultats/comptage.txt
 grep ">" ./databases/all_genome.fasta|cut -f 2 -d ">" > ./resultats/association.tsv
 
 
-# 3° 
+# 3° Assemblage du génome des bactéries avec MEGAHIT avec une seule taille
+#    de KMER 21
+./soft/megahit -1 EchG_R1.fastq -2 EchG_R2.fastq --k-max 21 --memory 0.4 -o ./resultats/res_megahit
+
+
+
